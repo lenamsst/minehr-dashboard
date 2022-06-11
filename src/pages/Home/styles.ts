3913f57afa48671a53bb419533ba244a676dcb0a
Milena <lenamsst@gmail.com>
2022-06-11 00:54:41 -0300

    feat: criando pagina home contendo components header, title, button

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.main`
+    max-width: 1440px;
+    margin: 0 auto;
+`;
+
+export const TitleWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 45px;
+    margin-bottom: 45px;
+`;
